5d1b15651c143012517eb69beeeba5550626533f
Abdurrahman Stjärnborg <apo.b@hotmail.se>
2024-09-04 14:31:44 +0200

    senast

--- tests/testSuit01.spec.ts
+++ tests/testSuit01.spec.ts
@@ -1,12 +1,16 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from './pages/login-page';
 import { DashboardPage } from './pages/dashboard-page';
+import { faker } from '@faker-js/faker';
+
+const randomName = faker.person.firstName();
+const randomPassword = faker.internet.password();
+
 
 test.describe('Test suite 01', () => {
   test('Test case 01', async ({ page }) => {
     const loginPage = new LoginPage(page);
     const dashboardPage = new DashboardPage(page);
-
     await loginPage.goto();
     await loginPage.performLogin(`${process.env.TEST_USERNAME}`, `${process.env.TEST_PASSWORD}`)
     await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
@@ -15,16 +19,29 @@ test.describe('Test suite 01', () => {
     await page.waitForTimeout(5000);
   });
 
-
-  test('Unsuccessful login ', async ({ page }) => {
+  test('Unsuccessful login by UserName', async ({ page }) => {
     const loginPage = new LoginPage(page);
     const dashboardPage = new DashboardPage(page);
 
+    // Generate a random username
+    const randomName = faker.internet.userName();
     await loginPage.goto();
-    await loginPage.performLogin(`${process.env.TEST_USERNAME}`, `${process.env.TEST_PASSWORD}`)
-    await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
-    await dashboardPage.performLogout();
-    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+    await loginPage.performLogin(randomName, `${process.env.TEST_PASSWORD}`);
     await page.waitForTimeout(5000);
+    await expect(page.getByText('Bad username or password')).toBeVisible();
   });
+
+
+  test('Unsuccessful login by Password', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const dashboardPage = new DashboardPage(page);
+
+    // Generate a random username
+    const randomName = faker.internet.password();
+    await loginPage.goto();
+    await loginPage.performLogin(`${process.env.TEST_USERNAME}`, randomPassword);
+    await page.waitForTimeout(5000);
+    await expect(page.getByText('Bad username or password')).toBeVisible();
+  });
+
 });
